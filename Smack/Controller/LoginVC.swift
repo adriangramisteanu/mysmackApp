47927cb5345474cf28f6e617b9d3362d8fcc7914
adrian.gramisteanu <adrian.gramisteanu@SL16233-220.softvision.ro>
2019-09-25 13:52:49 +0300

    Lesson 4

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by adrian.gramisteanu on 25/09/2019.
+//  Copyright © 2019 Adrian Gramisteanu. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by adrian.gramisteanu on 25/09/2019.
+//  Copyright © 2019 Adrian Gramisteanu. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
